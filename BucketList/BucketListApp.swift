b1ce8821c3971bf71fd3c46a91821f0e88746111
Andrei Korikov <kpanic666@gmail.com>
2021-09-14 00:45:06 +0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Andrei Korikov on 14.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
